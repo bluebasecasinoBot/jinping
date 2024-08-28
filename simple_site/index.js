4aebfaa01298ead38d9678898de79d2a945bf3a2
bluebasecasinoBot <developerme33@gmail.com>
2024-08-28 21:48:15 +0100

    Add files via upload

---
+++ simple_site/index.js
@@ -0,0 +1,258 @@
+import {__c,__SYD,__sS,__sC,__m,__g,__p, __u, __v} from './sydneyLib/sydneyDom.js';
+import "./index2.js"
+
+__sS([
+    {
+        nameTag:'container',
+        style:{
+            height:'100vh',
+            width:'100vw',
+            backgroundColor:'#AF1E21',
+            backgroundAttachment: 'fixed',
+            // backgroundImage:'url("./b2.png")',
+            overflowY:'scroll',
+            position:'relative',
+            fontFamily:'ubuntu',
+            // wordSpacing:'5px',
+            // fontFamily:'sunrise',
+            overflowX:'hidden',
+            color:'#fff'
+        }
+    },
+    {
+        nameTag:'navBar',
+        style:{
+            width:'100%',
+            display:'flex',
+            padding:'5px 10px',
+            columnGap:'30px',
+            // position:'sticky',
+            // top:'0',
+            zIndex:'999',
+            // background:'#791102',
+            alignItems:'center',
+            justifyContent:'space-between',
+            // borderBottom:'5px solid #f0472f',
+            borderBottom:'3px solid #fff'
+        }
+    },
+    {
+        nameTag:'_body_style',
+        style:{
+            minHeight:'100%',
+            height:'fit-content',
+            width:'100%',
+            display:'flex',
+            alignItems:'center',
+            flexDirection:'column',
+            alignItems:'center',
+            justifyContent:'center',
+            rowGap:'20px',
+            columnGap:'10px',
+            padding:'0 15px',
+            paddingTop:'50px',
+            textAlign:'center',
+            // transform:'translateY(-20px)'
+        }
+    }
+])
+
+
+__SYD.container = () =>{
+    return __c(
+        'div',
+        {
+            style:__sC.container()
+        },
+        [
+            __c(
+                'div',
+                {
+                    style:'height:fit-content;min-height:50px;width:100%;display:flex;justify-content:space-between;padding:5px 20px;align-items:center;border-bottom:3px solid #fff;'
+                },
+                [
+                    __c(
+                        'h2',
+                        {},
+                        [
+                            'PRESIDENT XI Jinping'
+                        ]
+                    ),
+                    __c(
+                        'div',
+                        {
+                            style:'height:fit-content;width:fit-content;display:flex;column-gap:10px;'
+                        },
+                        [
+                            __c(
+                                'h2',
+                                {
+                                    class:'btn_model'
+                                },
+                                [
+                                    __c('a',{target:'blank',href:'https://www.dextools.io/app/en/tron/pair-explorer/TAY4ju94nnZAYnYRQzkUjekK1Bbqzsip9L?t=1723950469399',style:'color:#fff'},['Chart'])
+                                ]
+                            ),
+                            __c(
+                                'h2',
+                                {
+                                    class:'btn_model'
+                                },
+                                [
+                                    __c('a',{target:'blank',href:'https://sunpump.meme/token/TR95WcjR78Y4puyv8BkbFuxaf4WcvmmPpd',style:'color:#fff'},['Buy '])
+                                ]
+                            ),
+                        ]
+                    )
+                ]
+            ),
+
+            __SYD._p2(),
+            __SYD.buy_section(),
+            __SYD.footer()
+        ],
+        {
+            genericStyle:['bg_fit']
+        }
+    )
+}
+
+
+__SYD.buy_section = () =>{
+    return __c(
+        'div',
+        {
+            style:'display:flex;flex-direction:column;row-gap:50px'
+        },
+        [
+            __c(
+                'h1',
+                {
+                    style:'width:100%;text-align:center;border-bottom:4px solid #fff;',
+                    class:'h_buy'
+                },
+                [
+                    'HOW TO BUY'
+                ]
+            ),
+            __c(
+                'div',
+                {
+                    style:'display:flex;justify-content:space-around;',
+                    class:'buy_box_cont'
+                },
+                [
+                    __c('div',
+                        {
+                            style:"position:relative;color:#171717;font-weight:500;",
+                            class:'box_buy_section animate1'
+                        },
+                        [
+                            __c(
+                                'div',
+                                {
+                                    style:'position:absolute;top:0;left:50%;transform:translateX(-50%) translateY(-30%);font-size:50px;',
+                                    class:"box_buy_section_child"
+                                },[
+                                    '1'
+                                ]
+                            ),
+                            __c('p',{},['Download TronLink wallet:',__c('a',{href:'https://tronlink.org'},['tronlink.org'])])
+                        ]
+                    ),
+                    __c('div',
+                        {
+                            style:"position:relative;color:#171717;font-weight:500;",
+                            class:'box_buy_section animate2'
+                        },
+                        [
+                            __c(
+                                'div',
+                                {
+                                    style:'position:absolute;top:0;left:50%;transform:translateX(-50%) translateY(-30%);font-size:50px;',
+                                    class:"box_buy_section_child"
+                                },[
+                                    '2'
+                                ]
+                            ),
+                            __c('p',{},['Bridge from ANY chain to TRON. Fast and easy using FixedFloat: ',__c('a',{href:'https://ff.io/'},['ff.io'])])
+                        ]
+                    ),
+                    __c('div',
+                        {
+                            style:"position:relative;color:#171717;font-weight:500;",
+                            class:'box_buy_section animate3'
+                        },
+                        [
+                            __c(
+                                'div',
+                                {
+                                    style:'position:absolute;top:0;left:50%;transform:translateX(-50%) translateY(-30%);font-size:50px;',
+                                    class:"box_buy_section_child"
+                                },[
+                                    '3'
+                                ]
+                            ),
+                            __c('p',{},['Buy $BIAO at ',__c('a',{href:'https://sunpump.meme/token/'},['https://sunpump.meme/token/'])])
+                        ]
+                    )
+                ]
+            )
+        ]
+    )
+}
+
+__SYD.footer = () =>{
+    return __c(
+        'div',
+        {
+            style:'height:500px;width:100%;background:#cab518;margin-top:50px;border:8px solid #000;border-radius:20px;display:flex;justify-content:center;align-items:center;flex-wrap:wrap;column-gap:30px;flex-wrap:wrap;row-gap:50px;'
+        },
+        [
+            __c(
+                'h2',
+                {
+                    style:'',
+                    class:'btn_model_dark'
+                },
+                [
+                    'TELEGRAM'
+                ]
+            ),
+
+            __c(
+                'h2',
+                {
+                    style:'',
+                    class:'btn_model_dark'
+                },
+                [
+                    'TWITTER'
+                ]
+            )
+        ]
+    )
+}
+
+// __SYD.page1 = () =>{
+//     return __c(
+//         'div',
+//         {
+//             style:__sC._body_style({
+//                 method:'add',style:{
+//                     height:'fit-content',
+//                     flexDirection:'row',
+//                     transform:'unset',
+//                     marginBottom:'20px',
+//                     alignItems:'flex-start',
+//                     flexWrap:'wrap'
+//                 }
+//             })
+//         },
+//         [
+
+//         ]
+//     )
+// }
+
+__m(__SYD.container())
---
+++ simple_site/index2.js
@@ -0,0 +1,202 @@
+import {__c,__SYD,__sS,__sC,__m,__g,__p, __u, __v, createElement} from './sydneyLib/sydneyDom.js';
+
+__sS(
+    {
+        nameTag:'_social_style',
+        style:{
+            minHeight:'60px',
+            minWidth:'80px',
+            cursor:'pointer',
+            border:'2px solid #fff'
+        }
+    }
+)
+
+__SYD._p2 = () =>{
+    return __c(
+        'div',
+        {
+            style:__sC._body_style({
+                method:'add',style:{
+                    height:'fit-content',
+                    flexDirection:__p(['_p2','_flex'],'row'),
+                    transform:'unset',
+                    marginBottom:'20px',
+                    alignItems:'flex-start',
+                    position:'relative'
+                }
+            })
+        },
+        [
+            __c(
+                'div',
+                {
+                    style:`min-height:fit-content;height:100%;width:${__p(['_p2','_flex'],'row') === 'row' ? '60%' : '100%'};background:transparent;display:flex;justify-content:center;align-items:center;flex-direction:column;row-gap:20px;z-index:300;padding-top:20%;`
+                },
+                [
+                    __c('h2',{class:'',style:`color:#ffb973;text-align:center;width:100%;font-size:${__p(['_p2','__font'],{2:'50px'})['2']}`},['PRESIDENT XI Jinping']),
+                    // __c('h1',{class:'',style:`font-family:sunrise;font-size:${__p(['_p2','__font'],{1:'70px'})['1']};`},['$JADE']),
+                    // __c('h2',{class:'',style:`color:#ffb973;text-align:center;width:100%;font-size:${__p(['_p2','__font'],{2:'50px'})['2']}`},['The Celestial Meme Coin on the Tron Blockchain!']),
+                    __SYD.about_text_p2(),
+                    __SYD.about_text_p3(),
+                    __SYD.about_text_p4(),
+                    __SYD.page1Socials(),
+                    __c(
+                        'div',
+                        {style:'display:flex;width:100%;row-gap:10px;align-items:center;text-align:left;height:100px;column-gap:20px'},
+                        [
+                            __c(
+                                'p',
+                                {
+                                    style:'font-size:22px;width:max-content;min-width:fit-content;text-align:left;font-weight:900;'
+                                },
+                                [
+                                    'C.A'
+                                ]
+                            ),
+                            __c(
+                                'div',
+                                {
+                                    style:'min-height:70px;min-width:70px;background-color:#d83f2a;cursor:pointer;display:flex;align-items:center;font-size:15px;padding:0 5px',
+                                    class:'btn_copy'
+                                },[
+                                    '-------------------------------------------'
+                                ],{genericStyle:['bg_fit']},
+                                {
+                                    events:{
+                                        onclick:() =>{
+                                            navigator.clipboard.writeText('0x1852B16e564f5413C45986D13442791B08C9D3B8')
+                                            // __v['c_a'].textContent = 'Contract Address Copied'
+                                            alert('contract address copied')
+
+                                            // const timer = setTimeout(() =>{
+                                            // __v['c_a'].textContent = '0x1852B16e564f5413C45986D13442791B08C9D3B8'
+                                            // clearTimeout(timer)
+                                            // },1500)
+                                        }
+                                    }
+                                }
+                            )
+                            // __c(
+                            //     'div',
+                            //     {
+                            //         style:`padding:50px 10px;width:100%;background:#884711;font-family:ubuntu;font-size:${__p(['_p2','_flex'],'row') === 'row' ? '20px' : '13px'};font-weight:900;text-align:center;cursor:pointer;border-radius:20px;box-shadow:1.5px 1.5px 0px 2px rgba(21,2,9);color:#fff;`
+                            //     },
+                            //     [
+                            //         '0x1852B16e564f5413C45986D13442791B08C9D3B8'
+
+                            //     ],
+                            //     {
+                            //         events:{
+                            //             onclick:() =>{
+                            //                 navigator.clipboard.writeText('0x1852B16e564f5413C45986D13442791B08C9D3B8')
+                            //                 __v['c_a'].textContent = 'Contract Address Copied'
+
+                            //                 const timer = setTimeout(() =>{
+                            //                 __v['c_a'].textContent = '0x1852B16e564f5413C45986D13442791B08C9D3B8'
+                            //                 clearTimeout(timer)
+                            //                 },1500)
+                            //             }
+                            //         },
+                            //         type:'c_a'
+                            //     }
+                            // )
+                        ]
+                    )
+                ]
+            ),
+            __c(
+                'div',
+                {
+                    style:`min-height:${__p(['_p2','_flex'],'row') === 'row' ? '100vh' : '300px'};width:${__p(['_p2','_flex'],'row') === 'row' ? '40%' : '100%'};border-radius:20px;background-image:url("./b2.png");background-size:contain;background-color:transparent;height:50%;width:100%;`//url("./assets/goatx.jpg")
+                },[],
+                {
+                    genericStyle:[__p(['_p2','_flex'],'row') === 'row' ? 'bg_cover' : 'bg_fit'],
+                }
+            ),
+        ],
+        {
+            createState:{
+                stateName:'_p2',
+                state:{_flex:'row',__font:{1:'70px',2:'50px'}}
+            },
+            mediaQuery:{
+                query:[{size:'<900px',prop:{_flex:'column-reverse',__font:{1:'30px',2:'20px'}}}],
+                defState:{_flex:'row',__font:{1:'70px',2:'50px'}}
+            }
+        }
+    )
+}
+
+__SYD.page1Socials = () =>{
+    return __c(
+        'div',
+        {
+            style:'width:100%;padding:10px 0;display:flex;justify-content:center;align-items:center;column-gap:20px;row-gap:20px;flex-wrap:wrap',
+        },
+        [
+            // __SYD.page1SocialBtn({contentSrc:'mm'}),
+            __SYD.page1SocialBtn({contentSrc:'./t.png'}),
+            __SYD.page1SocialBtn({contentSrc:'./tw.png'})
+        ]
+    )
+}
+
+__SYD.page1SocialBtn = ({contentSrc,link = '#'}) =>{
+    return __c(
+        'div',
+        {
+            style:__sC._social_style({method:'add',style:{backgroundImage:`url('${contentSrc}')`}}),
+            class:'click btn_buy' + contentSrc === './t.png' ? ' animate1' : ' animate2'
+        },
+        [],
+        {
+            genericStyle:['bg_fit'],
+            events:{
+                onclick:() =>{
+                    window.location = link
+                }
+            }
+        }
+    )
+}
+__SYD.freeText_def = (text) =>{
+    return __c(
+        'p',
+        {
+            style:'font-size:22px;width:50%;min-width:fit-content;text-align:center'
+        },
+        [
+            text
+        ]
+    )
+}
+__SYD.freeText_def_small = (text) =>{
+    return __c(
+        'p',
+        {
+            style:'font-size:16px;width:100%;text-align:center'
+        },
+        [
+            text
+        ]
+    )
+}
+
+__SYD.about_text_p2= () =>{
+    return __c('p',{style:'font-size: 1.5rem;line-height: 1.75rem;text-align:left;font-weight:500;width:100%;text-align:center;'},[
+        `Hey everyone as you all know I am xi Jinping a Chinese politician who has been the general secretary of the (CCP) and (CMC), and thus the paramount leader of China, since 2012. I have also been the president of the People's Republic of China (PRC) since 2013. As a member of the fifth generation of Chinese leadership, Xi is the first CCP general secretary born after the establishment of the PRC.`,
+    ])
+}
+
+__SYD.about_text_p3= () =>{
+    return __c('p',{style:'font-size: 1.5rem;line-height: 1.75rem;text-align:left;font-weight:500;width:100%;text-align:center;margin-top:5px;'},[
+        `What am I trying to say ? `,
+    ])
+}
+
+__SYD.about_text_p4 = () =>{
+    return __c('p',{style:'font-size: 1.5rem;line-height: 1.75rem;text-align:left;font-weight:500;width:100%;text-align:center;margin-top:5px;'},[
+        `It’s time to take over the tron space done it in chine and I’ll do it again`,
+    ])
+}
